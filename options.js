a753b68516d53c59804a1ad89ca645c8c9381981
Sebastian Gun <sebastiangun@gmail.com>
2012-02-17 23:07:40 -0300

    Happy new year

--- options.js
+++ options.js
@@ -2,3 +2,7 @@ function printVersion() {
 	// see http://www.opera.com/docs/apis/extensions/widgetobjectguide/
 	document.write(widget.version);
 }
+
+function printCurrentYear() {
+	document.write((new Date()).getFullYear());
+}
